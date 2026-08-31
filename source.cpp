de399b3d9d55439de05fb0bbf82bf056603559b4
Lana Vu <lananghivu@gmail.com>
2026-08-30 22:44:56 -0700

    Initial commit

---
+++ source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::string medication{""};
+    int count{};
+    int intervals{};
+    std::cout << "Enter medication: ";
+    std::cin >> medication;
+
+    std::cout << "\nEnter amount of times needed per day: ";
+    std::cin >> count;
+
+    std::cout << "\nEnter intervals between medication: ";
+    std::cin >> intervals;
+
+    
+
+
+
+}
